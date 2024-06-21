da2488d7d4eeae7c81be1d4aead9d9cff801621e
Ayobamiu <ayobamiu@gmail.com>
2024-06-20 23:33:07 -0400

    Added _redirects

--- src/Components/EntryList.tsx
+++ src/Components/EntryList.tsx
@@ -25,17 +25,26 @@ const EntryList: React.FC = () => {
       <div className="max-w-3xl mx-auto p-4 ">
         <List
           header={
-            <h1 className="text-2xl font-bold">Teams' members request</h1>
+            <h1 className="text-2xl font-bold dark:text-gray-400">
+              Teams' members request
+            </h1>
           }
           bordered
           itemLayout="vertical"
           dataSource={entries}
           loading={loading}
+          className="dark:text-gray-400"
           renderItem={(item) => (
             <List.Item
               actions={[
-                <a key="list-loadmore-edit">☎️ Phone: {item.phone}</a>,
-                <a href={`tel:${item.phone}`} key="list-loadmore-more">
+                <a className="dark:text-gray-400" key="list-loadmore-edit">
+                  ☎️ Phone: {item.phone}
+                </a>,
+                <a
+                  className="dark:text-gray-400"
+                  href={`tel:${item.phone}`}
+                  key="list-loadmore-more"
+                >
                   Call {item.name}
                 </a>,
               ]}
@@ -43,17 +52,23 @@ const EntryList: React.FC = () => {
               <List.Item.Meta
                 title={
                   <div>
-                    <h1>Posted by: {item.name}</h1>
-                    <h1>Team: {item.teamName}</h1>
+                    <h1 className="dark:text-gray-400">
+                      Posted by: {item.name}
+                    </h1>
+                    <h1 className="dark:text-gray-400">
+                      Team: {item.teamName}
+                    </h1>
                   </div>
                 }
-                description={item.message}
+                description={
+                  <p className="dark:text-gray-400">{item.message}</p>
+                }
               />
-              <div>
+              <div className="dark:text-gray-400">
                 Skills we are looking for:{" "}
                 {(item.desired_skills || []).join(", ")}
               </div>
-              <div>
+              <div className="dark:text-gray-400">
                 SDGs of Interest: &nbsp;
                 <Space wrap>
                   {(item.sdgsOfInterest || []).map((sdg) => {
