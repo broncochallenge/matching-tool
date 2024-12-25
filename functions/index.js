fc880652774b74f79c7349569caceeb2736639e6
Ayobamiu <ayobamiu@gmail.com>
2024-12-24 22:26:50 -0500

    removed firebase config for functions

--- functions/index.js
+++ functions/index.js
@@ -12,18 +12,7 @@ const logger = require("firebase-functions/logger");
 const admin = require("firebase-admin");
 const base64 = require("base-64");
 
-const firebaseConfig = {
-  apiKey: process.env.REACT_APP_APIKEY,
-  authDomain: process.env.REACT_APP_AUTHDOMAIN,
-  databaseURL: process.env.REACT_APP_DATABASE_URL,
-  projectId: process.env.REACT_APP_PROJECTID,
-  storageBucket: process.env.REACT_APP_STORAGEBUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
-  appId: process.env.REACT_APP_APPID,
-  measurementId: process.env.REACT_APP_MEASUREMENTID,
-};
-
-admin.initializeApp(firebaseConfig);
+admin.initializeApp();
 const db = admin.firestore();
 
 // Create and deploy your first functions
